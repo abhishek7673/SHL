7af4524f8066353ccb2585ff7e4b7ea95ffac78f
Abhishek Kumar <abhishek957dss@gmail.com>
2025-04-07 01:09:55 +0530

    update return

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ async def get_recommendations(request: Request):
     body = await request.json()
     query = body.get("query")
     results = recommender.recommend(query)
-    return results.drop(columns="score").to_dict(orient="records")
+    return results.to_dict(orient="records")
 
 if __name__ == "__main__":
     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
